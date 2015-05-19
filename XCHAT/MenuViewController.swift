2df5c554b784253d33be0fc7dba87393926f5bc7
Jim <caijim@github.com>
2015-05-18 22:01:34 -0700

    squashed into one

---
+++ XCHAT/MenuViewController.swift
@@ -0,0 +1,91 @@
+//
+//  MenuViewController.swift
+//  XCHAT
+//
+//  Created by Mateo Garcia on 5/13/15.
+//  Copyright (c) 2015 Mateo Garcia. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var hamburgerViewController: HamburgerViewController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.estimatedRowHeight = 44.0
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.tableFooterView = UIView(frame: CGRectZero)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: Table View
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        switch indexPath.row {
+        case 0:
+            var cell = tableView.dequeueReusableCellWithIdentifier("ProfileCell") as UITableViewCell
+            return cell
+        case 1:
+            var cell = tableView.dequeueReusableCellWithIdentifier("ThreadsCell") as UITableViewCell
+            return cell
+        case 2:
+            var cell = tableView.dequeueReusableCellWithIdentifier("CalendarCell") as UITableViewCell
+            return cell
+        case 3:
+            var cell = tableView.dequeueReusableCellWithIdentifier("ReelCell") as UITableViewCell
+            return cell
+        case 4:
+            var cell = tableView.dequeueReusableCellWithIdentifier("MembersCell") as UITableViewCell
+            return cell
+        default:
+            var cell = tableView.dequeueReusableCellWithIdentifier("SettingsCell") as UITableViewCell
+            return cell
+        }
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        switch indexPath.row {
+        default:
+            var profileViewController = storyboard!.instantiateViewControllerWithIdentifier("ProfileViewController") as ProfileViewController
+            
+            // SETUP PROFILE
+            
+            hamburgerViewController?.contentViewController = profileViewController
+//        default:
+//            var chatNavigationController = storyboard!.instantiateViewControllerWithIdentifier("ChatNavigationController") as! UINavigationController
+//            var chatViewController = chatNavigationController.viewControllers[0] as! ChatViewController
+//            hamburgerViewController?.contentViewController = chatNavigationController
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- XCHAT/XCHAT-Bridging-Header.h
+++ XCHAT/XCHAT-Bridging-Header.h
@@ -5,3 +5,4 @@
 # import "PFAnalytics.h"
 # import "PFInstallation.h"
 
+#import <Parse/Parse.h>
